a96ba170a1d63e91e4d66fd7ad5d16983a60749c
Ruiz-M-Phang <Ruiz.M.Phang@gmail.com>
2026-09-04 16:50:11 +0200

    De test van sprint 2

--- Alarm.bat
+++ Alarm.bat
@@ -2,13 +2,13 @@
 
 echo You're Alarm will go off in:
 @REM Dit is de timer wanneer het op 0 staat dan word regel 6 uitgevoerd
-timeout 3600
-@REM Mijn gekozen liedje https://music.youtube.com/watch?v=4QQ6nNShp34
-start https://music.youtube.com/watch?v=4QQ6nNShp34
-@REM 3 reminders
+timeout 5
+@REM Mijn gekozen liedje Speed_Up_(Radio Mix).mp3 die afgespeeld wordt in vlc afgespeeld 
+start vlc.exe "Speed_Up_(Radio Mix).mp3"
+@REM 3 reminders beide 5 minuten
 timeout 900
-start https://music.youtube.com/watch?v=4QQ6nNShp34
+start vlc.exe "Speed_Up_(Radio Mix).mp3"
 timeout 900
-start https://music.youtube.com/watch?v=4QQ6nNShp34
+start vlc.exe "Speed_Up_(Radio Mix).mp3"
 timeout 900
-start https://music.youtube.com/watch?v=4QQ6nNShp34
+start vlc.exe "Speed_Up_(Radio Mix).mp3"
